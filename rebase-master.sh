a1ed2f42a7c3cc30716129d787353b7efc0950fd
Gareth <bravdthepally@gmail.com>
2022-12-31 19:41:46 +0200

    WIP

--- rebase-master.sh
+++ rebase-master.sh
@@ -2,10 +2,22 @@
 
 WORKING_PATH=$(pwd)
 
-printf "Rebasing dev to master in the following directory: %s\n" "$WORKING_PATH"
+MASTER="master"
+DEV="dev"
 
-git -C "$WORKING_PATH" checkout master || exit 1
+while getopts ":m:d:" opt; do
+  echo $OPTARG
+  case $opt in
+    m) MASTER=$OPTARG;;
+    d) DEV=$OPTARG;;
+    *) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+  esac
+done
+
+printf "Rebasing $DEV to $MASTER in the following directory: %s\n" "$WORKING_PATH"
+
+git -C "$WORKING_PATH" checkout $MASTER || exit 1
 git -C "$WORKING_PATH" pull || exit 1
-git -C "$WORKING_PATH" checkout dev || exit 1
-git -C "$WORKING_PATH" rebase master || exit 1
+git -C "$WORKING_PATH" checkout $DEV || exit 1
+git -C "$WORKING_PATH" rebase $MASTER || exit 1
 git -C "$WORKING_PATH" push || exit 1
