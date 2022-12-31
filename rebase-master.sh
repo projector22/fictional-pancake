c839b4bce0ec6584ddea0b3bf61cb6cd6e0b7653
Gareth <bravdthepally@gmail.com>
2022-12-31 19:30:45 +0200

    Fixed bug with pwd

--- rebase-master.sh
+++ rebase-master.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-WORKING_PATH=pwd
+WORKING_PATH=$(pwd)
 
-git -C $WORKING_PATH checkout master
-git -C $WORKING_PATH pull
-git -C $WORKING_PATH checkout dev
-git -C $WORKING_PATH rebase master
-git -C $WORKING_PATH push
+printf "Rebasing dev to master in the following directory: %s\n" "$WORKING_PATH"
+
+git -C "$WORKING_PATH" checkout master || exit 1
+git -C "$WORKING_PATH" pull || exit 1
+git -C "$WORKING_PATH" checkout dev || exit 1
+git -C "$WORKING_PATH" rebase master || exit 1
+git -C "$WORKING_PATH" push || exit 1
